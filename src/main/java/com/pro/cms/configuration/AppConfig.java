982a67a777f4595857a6340a1e96e377759c9ce6
Roshan Ranabhat <roshanr@ekbana.com>
2017-11-17 17:36:28 +0545

    MessageSource configure for custom validation message

--- src/main/java/com/pro/cms/configuration/AppConfig.java
+++ src/main/java/com/pro/cms/configuration/AppConfig.java
@@ -1,9 +1,51 @@
 package com.pro.cms.configuration;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 @ComponentScan({"com.pro.cms"})
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurationSupport{
+    /**
+     * Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+
+    /**
+     * Configure MessageSource to lookup any validation/error message in internationalized property files
+     */
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+    /**
+     * Optional. It's only required when handling '.' in @PathVariables which otherwise ignore everything after last '.' in @PathVaidables argument.
+     * It's a known bug in Spring [https://jira.spring.io/browse/SPR-6164], still present in Spring 4.1.7.
+     * This is a workaround for this issue.
+     */
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseRegisteredSuffixPatternMatch(true);
+    }
+
+
+    @Bean(name="multipartResolver")
+    public StandardServletMultipartResolver resolver(){
+        return new StandardServletMultipartResolver();
+    }
 }
--- src/main/java/com/pro/cms/configuration/AppIitializer.java
+++ src/main/java/com/pro/cms/configuration/AppIitializer.java
@@ -2,6 +2,8 @@ package com.pro.cms.configuration;
 
 import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletRegistration;
 
 public class AppIitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
@@ -23,7 +25,25 @@ public class AppIitializer extends AbstractAnnotationConfigDispatcherServletInit
     }
 
     @Override
-    protected void customizeRegistration(ServletReg registration) {
-        super.customizeRegistration(registration);
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(getMultipartConfigElement());
     }
+
+    private MultipartConfigElement getMultipartConfigElement(){
+        MultipartConfigElement multipartConfigElement = new MultipartConfigElement(LOCATION, MAX_FILE_SIZE, MAX_REQUEST_SIZE, FILE_SIZE_THRESHOLD);
+        return multipartConfigElement;
+    }
+
+
+    /*upload variables*/
+
+    //private static final String LOCATION = "/Applications/MAMP/htdocs/tmp";
+    private static final String LOCATION = "/home/alis/tmp";
+
+    private static final long MAX_FILE_SIZE = 1024 * 1024 * 25;//25MB
+
+    private static final long MAX_REQUEST_SIZE = 1024 * 1024 * 30;//30MB
+
+    private static final int FILE_SIZE_THRESHOLD = 0;
+
 }
